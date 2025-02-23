f763588e22bd057be09790bcc884fce0b2c8c44b
Anand raja <meetanandns@gmail.com>
2025-02-23 17:14:51 +0000

    basic nav bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-angular-parcel',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
